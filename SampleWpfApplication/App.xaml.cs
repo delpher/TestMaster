68f9968fb7dbc77549aa93d9689f920b1d2133ac
delpher <a.onashyuk@itu.int>
2023-04-15 16:47:22 +0300

    Implemented simple HttpListener server for hosting test endpoints

---
+++ SampleWpfApplication/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Windows;
+
+namespace SampleWpfApplication
+{
+    public partial class App
+    {
+        private readonly CancellationTokenSource _cts;
+
+        public App()
+        {
+            _cts = new CancellationTokenSource();
+        }
+        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            TestingEndpointServer.Start(_cts.Token);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _cts.Cancel();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ SampleWpfApplication/TestingEndpointServer.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Threading;
+
+namespace SampleWpfApplication;
+
+public static class TestingEndpointServer
+{
+    private static readonly Thread ServerThread;
+
+    public static void Start(CancellationToken cancellationToken)
+    {
+        ServerThread.Start(cancellationToken);
+    }
+
+    static TestingEndpointServer()
+    {
+        ServerThread = new Thread(RunHttpServer);
+    }
+
+    private static void RunHttpServer(object obj)
+    {
+        var ct = (CancellationToken)obj;
+        var server = new HttpListener();
+        server.Prefixes.Add("http://localhost:8080/");
+
+        ct.Register(() => server.Stop());
+            
+        server.Start();
+
+        while (!ct.IsCancellationRequested)
+        {
+            var context = server.GetContext();
+            ThreadPool.QueueUserWorkItem(_ => HandleRequest(context));
+        }
+    }
+
+    private static void HandleRequest(HttpListenerContext context)
+    {
+        var buffer = "{\"test\":\"test_value\"}"u8.ToArray();
+        context.Response.ContentType = "application/json";
+        context.Response.ContentLength64 = buffer.Length;
+        context.Response.OutputStream.Write(buffer);
+        context.Response.OutputStream.Flush();
+    }
+}
